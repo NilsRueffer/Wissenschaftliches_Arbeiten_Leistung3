26d25437235456297a8f6f933539d9010669651c
Tatascie <thao.huynh@tu-dortmund.de>
2021-02-17 22:46:17 +0100

    Added function for statistics of categorial variables.

--- script1.R
+++ script1.R
@@ -4,3 +4,8 @@ statistics_for_metric_variables <- function(variables) {
   # TODO calculate and return the following : 
   # mean, median, variance/standard deviation, covariance-matrix, maximum, minimum, ...
 }
+
+statistics_for_categorial_variables <- function(variables) {
+  # TODO calculate and return the following : 
+  # bincounts, max-count, min-count, ...
+}
