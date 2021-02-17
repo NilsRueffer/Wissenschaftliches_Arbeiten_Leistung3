789688ca00ddc953a2fde283c07943942e599e50
Tatascie <thao.huynh@tu-dortmund.de>
2021-02-17 23:47:48 +0100

    Added function between two categorial variables and function between metric and dichotomous variables.

--- script1.R
+++ script1.R
@@ -1,11 +1,28 @@
 # Skript 1
-
+#1
 statistics_for_metric_variables <- function(variables) {
   # TODO calculate and return the following : 
   # mean, median, variance/standard deviation, covariance-matrix, maximum, minimum, ...
 }
 
+#2
 statistics_for_categorial_variables <- function(variables) {
   # TODO calculate and return the following : 
   # bincounts, max-count, min-count, ...
 }
+
+#3
+statistics_between_two_categorial_variables <- function(variables1, variables2) {
+  # TODO calculate and return the following : 
+   
+}
+
+#4
+statistics_between_metric_und_dichotomous_variables <- function(metricVariables, dichotomousVariables) {
+  # TODO calculate and return the following : 
+ 
+}
+
+#5
+
+#6
