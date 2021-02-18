248c0436b94c1d7a80f809642457e55e4e2307b8
UweTUDO <uwe.dressler@tu-dortmund.de>
2021-02-18 20:39:20 +0100

    Update script2.R
    Funktion read_data_in_df in Skript 2 aufgenommen

--- script2.R
+++ script2.R
@@ -1,3 +1,13 @@
+# Wissenschaftliches_Arbeiten_Leistung3
 # Skript 2
-# Helfer-Functions -> internal Functions of Script 1 Funtions  
+# Beschreibung
+# In dieser Datei sollen die Funktionen zusammengefasst werden, die von den Funktionen aus 
+# Skript 1 als Helferfunktion verwendet werden sollen
+# Version    Autor          Bemerkung
+# 1.0        Uwe Dreßler    Init
+#                           read_data_in_df hinzugefuehgt
 
+read_data_in_df <- function() {
+Datensatz <- read.csv("Datensatz.csv", header=TRUE)
+Datensatz_df <- data.frame(Datensatz)
+}
