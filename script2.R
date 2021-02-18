796268de096f3a64d674b141496457a8c2a0e886
UweTUDO <uwe.dressler@tu-dortmund.de>
2021-02-18 20:42:37 +0100

    Update script2.R
    Beschreibung geaendert

--- script2.R
+++ script2.R
@@ -3,10 +3,10 @@
 # Beschreibung
 # In dieser Datei sollen die Funktionen zusammengefasst werden, die von den Funktionen aus 
 # Skript 1 als Helferfunktion verwendet werden sollen
-# Version    Autor          Bemerkung
-# 1.0        Uwe Dreßler    Init
-#                           read_data_in_df hinzugefuehgt
-
+# Version    Autor          Datum   Bemerkung
+# 1.0        Uwe Dreßler    18.2.21 Init
+#                                   read_data_in_df hinzugefuehgt
+#
 read_data_in_df <- function() {
 Datensatz <- read.csv("Datensatz.csv", header=TRUE)
 Datensatz_df <- data.frame(Datensatz)
