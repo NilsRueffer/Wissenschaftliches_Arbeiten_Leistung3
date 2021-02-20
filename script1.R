728652952f0e1ac4f2a687ed2ca6664951134c24
UweTUDO <uwe.dressler@tu-dortmund.de>
2021-02-20 22:35:44 +0100

    Call der Helferfunktion zum einlesen der Daten
    Eintragung Aufgaben und Aufruf der Helferfunktion

--- script1.R
+++ script1.R
@@ -1,8 +1,14 @@
 # Skript 1
 #1
 statistics_for_metric_variables <- function(variables) {
+  # Version    Autor          Bemerkung
+  # 1.0        Mattias Keppke Init
+  # 2.0        Uwe Dreßler    Aufnahme Aufruf Dateneinlesen
+  
+  # Description
   # TODO calculate and return the following : 
   # mean, median, variance/standard deviation, covariance-matrix, maximum, minimum, ...
+  data <- read_data_in_df()
   metstat = list("Range Alter" = range(data$Alter),
                  "Mean Alter" = mean(data$Alter),
                  "Var Alter" = sd(data$Alter),
@@ -15,8 +21,14 @@ statistics_for_metric_variables <- function(variables) {
 
 #2
 statistics_for_categorial_variables <- function(variables) {
+  # Version    Autor          Bemerkung
+  # 1.0        Mattias Keppke Init
+  # 2.0        Uwe Dreßler    Aufnahme Aufruf Dateneinlesen
+  
+  # Description
   # TODO calculate and return the following : 
   # bincounts, max-count, min-count, ...
+  data <- read_data_in_df()
   katstat = list("Range Interesse an Mathe" = range(data$Interesse.an.Mathe),
                  "Mean Interesse an Mathe " = mean(data$Interesse.an.Mathe),
                  "Range Interesse an Programmieren" = range(data$Interesse.an.Programmieren),
